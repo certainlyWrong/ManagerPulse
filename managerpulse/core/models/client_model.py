89175704c516a45fed5b1aca98cb4c76ad5108ca
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2023-12-14 02:13:57 -0300

    Update ClientModel and ProductModel

--- managerpulse/core/models/client_model.py
+++ managerpulse/core/models/client_model.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field
 
 
 class ClientModel(BaseModel):
-    name: Optional[str] = Field(default=None)
+    name: str
     phone: Optional[str] = Field(default=None)
     email: Optional[str] = Field(default=None)
     address: Optional[str] = Field(default=None)
--- managerpulse/core/models/product_model.py
+++ managerpulse/core/models/product_model.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from pydantic import BaseModel, Field
 
 
-class ProductModel(SQLModel):
+class ProductModel(BaseModel):
     name: str
-    price: float
-    quantity: int
-    description: Optional[str]
+    price: float = Field(default=0.0)
+    quantity: int = Field(default=0)
+    description: Optional[str] = Field(default=None)
     created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: Optional[datetime]
+    updated_at: Optional[datetime] = Field(default=None)
